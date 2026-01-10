1dd4b8c3403ae52dab275a97ba91e5741761654f
nadahassan15 <nada2206712@miuegypt.edu.eg>
2026-01-10 04:15:41 +0200

    Update main.dart

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -44,8 +44,8 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const LoginScreen(),
-      // home: const HomeScreen(),
+      // home: const LoginScreen(),
+      home: const HomeScreen(),
       // home: const BrandHomeScreen(
       //   brandId: '947961b0-af07-4892-9a15-5e8f85eb1120',
       // ),
